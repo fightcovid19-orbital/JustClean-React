ffed172d12dd741e96491ca8628639ad658c21f9
WayneTan <waynetan2000@gmail.com>
2020-06-09 23:56:26 +0800

    feed

---
+++ src/components/Cleaner.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import withStyle from '@material-ui/core/styles/withStyles'
+import Link from 'react-router-dom/Link'
+
+// MUI stuff
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+
+const style = {
+    card: {
+        display: 'flex',
+        marginBottom: 20
+    },
+    image: {
+        minWidth: 200
+    },
+    content: {
+        padding: 25,
+        objectFit: 'cover'
+    }
+}
+
+class Cleaner extends Component {
+    render() {
+        const { classes, cleaner: { imageUrl, cleanerName, likeCount, unlikeCount, hiredCount } } = this.props
+        return (
+            <Card className={classes.card}>
+                <CardMedia image={imageUrl} title="Profile Image" className={classes.image} />
+                <CardContent className={classes.content}>
+                    <Typography variant="h5" component={Link} to={`/cleaners/${cleanerName}`} color='primary'>
+                        {cleanerName}
+                    </Typography>
+                </CardContent>
+            </Card>
+        )
+    }
+}
+
+export default withStyle(style)(Cleaner)
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,38 @@
 
 import React, { Component } from 'react'
+import axios from 'axios'
+import Cleaner from '../components/Cleaner'
+
+// MUI stuff
+import Grid from '@material-ui/core/Grid'
 
 class home extends Component {
+    state = {
+        cleaners: null
+    }
+    componentDidMount() {
+        axios.get('/cleaners')
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    cleaners: res.data
+                })
+            })
+            .catch(err => console.log(err))
+    }
     render() {
+        let recentCleanersMarkup = this.state.cleaners ? (this.state.cleaners.map(cleaner => <Cleaner cleaner={cleaner} />))
+            : <p>Loading...</p>
         return (
-            <div>
-                home page
-            </div>
+            <Grid container spacing={16}>
+                <Grid item sm={4} xs={12}>
+                    Profile...
+                </Grid>
+                <Grid item sm={8} xs={12}>
+                    {recentCleanersMarkup}
+                </Grid>
+
+            </Grid>
         )
     }
 }
